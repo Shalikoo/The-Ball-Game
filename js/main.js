c091688fff51fd463c5913035921b6f7edec317f
Shalev Azulay <shalev3238@gmail.com>
2025-03-18 16:30:57 +0200

    Add new ball and change onBallClick

--- js/main.js
+++ js/main.js
@@ -2,18 +2,16 @@
 
 var ballSize = 100
 
-function onBallClick() {
-    var elBall = document.querySelector('.ball')
-    
+function onBallClick(elBall) {
+
     ballSize += getRandomInt(20, 60)
 
     if(ballSize >= 400){
         ballSize = 100
-    } 
+    }
     
     elBall.style.width = ballSize + 'px'
     elBall.style.height = ballSize + 'px'
-    elBall.innerText = ballSize
     elBall.style.backgroundColor = getRandomColor()
-
+    elBall.innerText = ballSize
 }
