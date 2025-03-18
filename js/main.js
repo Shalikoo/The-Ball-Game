be9cafdd674a2ddbc9eb7a335d9e5195fc908834
Shalev Azulay <shalev3238@gmail.com>
2025-03-18 15:05:05 +0200

    removed console log

--- js/main.js
+++ js/main.js
@@ -3,11 +3,9 @@
 var ballSize = 100
 
 function onBallClick() {
-    console.log(`Ball is clicked!`)
     var elBall = document.querySelector('.ball')
 
     ballSize += 50
     elBall.style.width = ballSize + 'px'
     elBall.style.height = ballSize + 'px'
-    console.log(elBall.style.height)
 }
