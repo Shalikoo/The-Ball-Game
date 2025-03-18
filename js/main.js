d178fd61da224b2045490e84e149b3f26467f23b
Shalev Azulay <shalev3238@gmail.com>
2025-03-18 15:16:43 +0200

    Added if() to check ballSize not over 400px and reset to 100px when hits 400px

--- js/main.js
+++ js/main.js
@@ -5,8 +5,18 @@ var ballSize = 100
 function onBallClick() {
     var elBall = document.querySelector('.ball')
 
-    ballSize += 50
-    elBall.style.width = ballSize + 'px'
-    elBall.style.height = ballSize + 'px'
-    elBall.innerText = ballSize
+    if(ballSize >= 400){
+        elBall.style.width = 100 + 'px'
+        elBall.style.height = 100 + 'px'
+        ballSize = 100
+        elBall.innerText = ballSize
+    } else {
+        ballSize += 50
+        elBall.style.width = ballSize + 'px'
+        elBall.style.height = ballSize + 'px'
+        elBall.innerText = ballSize
+    }
+
+    
+
 }
